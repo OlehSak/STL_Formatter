cc195dbc93b4df4dec5b829cab84e607293783d1
Oleh Sak <oleh@digia.co>
2020-10-18 14:10:24 +0300

    angles calculation

--- main.cpp
+++ main.cpp
@@ -17,6 +17,30 @@ std::vector<std::string> split(const std::string& s, char delimiter)
 	return tokens;
 }
 
+double	vectorMultiple(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+	return x1 * x2 + y1 * y2 + z1 * z2;
+}
+
+double	vectorLength(double x, double y, double z)
+{
+	return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+}
+
+double	angleCalculetion(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+	const double PI = 3.141592653589793238463;
+	double mul;
+	double length1;
+	double length2;
+
+	mul = vectorMultiple(x1, y1, z1, x2, y2, z2);
+	length1 = vectorLength(x1, y1, z1);
+	length2 = vectorLength(x2, y2, z2);
+
+	return ((acos(mul / (length1 * length2)) * 180) / PI) - 90;
+}
+
 double	getSpace(
 	std::vector<double> vertex0,
 	std::vector<double> vertex1,
@@ -60,10 +84,13 @@ int main(int argc, char **argv)
 		std::vector<std::string>	splited;
 		double space;
 		double sum;
+		std::vector<double> facet;
+
 		std::vector<double> vertex0;
 		std::vector<double> vertex1;
 		std::vector<double> vertex2;
 
+		facet.resize(3);
 		vertex0.resize(3);
 		vertex1.resize(3);
 		vertex2.resize(3);
@@ -77,7 +104,14 @@ int main(int argc, char **argv)
 			if (found != std::string::npos)
 			{
 				splited = split(str, ' ');
-				//std::cout << "SPLITED FACET:" << splited.size() << ' ' << splited[4] << std::endl;
+				std::cout << "SPLITED FACET:" << splited.size() << ' ' << splited[4] << std::endl;
+
+				facet.at(0) = stod(splited[splited.size() - 3]);
+				facet.at(1) = stod(splited[splited.size() - 2]);
+				facet.at(2) = stod(splited[splited.size() - 1]);
+
+				double angle = angleCalculetion(facet[0], facet[1], facet[2], 0, 0, 1);
+				std::cout << "ANGLE in DEGREES:" << angle << std::endl;
 
 				std::getline(file, str);
 				std::string key("outer loop");
